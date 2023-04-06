3d457fecd4ef51b29257676c86c5da1a74e319cb
Minov Vitaliy <minov.vitaliy@tirolhelpsukraine.eu>
2023-04-06 19:13:55 +0200

    exercises on different css selectors

--- src/route/index.js
+++ src/route/index.js
@@ -3899,5 +3899,26 @@ router.get('/shopcatalog', function (req, res) {
 
 // ================================================================
 
+// ================================================================
+
+// router.get Створює нам один ентпоїнт
+
+//           ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/selector1', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('selector1', {
+    layout: 'default',
+    page: {
+      title: 'Vitaliy Minov',
+      subtitle: 'Resume project',
+    },
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
 // Підключаємо роутер до бек-енду
 module.exports = router
